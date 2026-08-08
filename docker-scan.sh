09def4cfdb1927afacb0c75c91b12d53e2915f5f
gcastrovInvenio <gcastrov@student.invenio.ac.cr>
2026-08-08 14:35:38 -0600

    Agrega configuracion para que solo falle si encuentra vulnerabilidades tipo high o critical

--- docker-scan.sh
+++ docker-scan.sh
@@ -3,16 +3,18 @@
 set -e
 
 IMAGE="${1:-meg-backend}"
+CVES_FILE=cves-logs.report
+SBOM_FILE=sbom-logs.sbom
 
 echo "Analizando la imagen '$IMAGE' con Docker Scout..."
 
-echo "-> Generando reporte de CVEs (cves-logs)..."
-docker scout cves --output cves-logs.report "$IMAGE"
+echo "-> Generando SBOM ($SBOM_FILE)..."
+docker scout sbom --output $SBOM_FILE "$IMAGE"
 
-echo "-> Generando SBOM (sbom-logs)..."
-docker scout sbom --output sbom-logs.sbom "$IMAGE"
+echo "-> Generando reporte de CVEs ($CVES_FILE)..."
+docker scout cves --only-severity critical,high --exit-code --output $CVES_FILE "$IMAGE"
 
 echo
-echo "Analisis completado:"
-echo "  CVEs: cves-logs"
-echo "  SBOM: sbom-logs"
+echo "Analisis completado sin vulnerabilidades criticas o altas:"
+echo "  CVEs: $CVES_FILE"
+echo "  SBOM: $SBOM_FILE"
